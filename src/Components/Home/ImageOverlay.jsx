66d162c76976fc96a2201036e7f73528c271b5b4
Akam <akamhiwa90@gmail.com>
2024-07-26 01:15:49 +0300

    removed a useless class

--- src/Components/Home/ImageOverlay.jsx
+++ src/Components/Home/ImageOverlay.jsx
@@ -1,6 +1,6 @@
 const ImageOverlay = () => {
   return (
-    <div className='overlay  rounded-[16px] absolute top-0 bottom-0 left-0 right-0 bg-GradientBG opacity-40'></div>
+    <div className='rounded-[16px] absolute top-0 bottom-0 left-0 right-0 bg-GradientBG opacity-40'></div>
 
   )
 }
