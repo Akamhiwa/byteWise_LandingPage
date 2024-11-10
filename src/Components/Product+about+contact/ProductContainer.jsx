10664363d6cfccc6930873f2343bd07534288f8d
Akam <akamhiwa90@gmail.com>
2024-11-10 19:50:55 +0300

    added product animation scroll

--- src/Components/Product+about+contact/ProductContainer.jsx
+++ src/Components/Product+about+contact/ProductContainer.jsx
@@ -52,11 +52,7 @@ const ProductContainer = () => {
   ]
   return (
     <div className="z-40 lg:pb-[200px] pb-[100px]  2xl:px-[220px] xl:px-[180px] lg:px-[100px] md:px-[30px] px-[20px] lg:-mt-24 -mt-16">
-      <div className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 lg:gap-y-[44px] content-center lg:gap-x-[40px] gap-[22px]">
-        {data.map((item,index)=>(
-          <Items item={item} key={index}/>
-        ))}
-      </div>
+      <Items images={data}/>
     </div>
   )
 }
